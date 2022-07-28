10bc84cf4ea57ad6f0079fe703147549b834c6d4
Roger Massana <rogermassana2002@gmail.com>
2022-07-28 17:55:15 +0200

    Calculator now operates 2 numbers

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const items = document.querySelectorAll('.item');
 const input = document.querySelector('#input');
-let dispValue = '';
+let dispValue = '', operation = '';
+let updateDispValue = 1;
 items.forEach(dispToInput);
 
 function add(a, b) {
@@ -22,13 +23,24 @@ function divide(a, b) {
 function operate(operator, a, b) {
     if (operator === '+') return add(a, b);
     if (operator === '-') return subtract(a, b);
-    if (operator === '*') return multiply(a, b);
+    if (operator === 'x') return multiply(a, b);
     if (operator === '/') return divide(a, b);
 }
 
 function dispToInput(el) {
     el.addEventListener('click', function() {
-        input.value = input.value + this.textContent;
-        dispValue = input.value;
+        if (el.classList.contains('operator')) {
+            if (el.innerHTML === '=') {
+                input.value = operate(operation, Number(dispValue), Number(input.value));
+                dispValue = '';
+            } else {
+                input.value = '';
+                operation = el.textContent;
+                updateDispValue = 1 - updateDispValue;
+            }
+        } else {
+            input.value = input.value + this.textContent;
+            if(updateDispValue) dispValue = input.value;
+        }
     });
 }
