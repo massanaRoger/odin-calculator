227983b9cca6b640a64ec2986e1e7861f09099cf
Roger Massana <rogermassana2002@gmail.com>
2022-07-28 20:08:55 +0200

    Fix clear button and error divide by 0

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-const items = document.querySelectorAll('.item');
-const input = document.querySelector('#input');
-let dispValue = '', operation = '';
-let updateDispValue = true, resetNext = false, cannotEquals = true;
-items.forEach(dispToInput);
+start();
+
+function start() {
+    const items = document.querySelectorAll('.item');
+    const input = document.querySelector('#input');
+    input.value = '';
+    let dispValue = '', operation = '';
+    let updateDispValue = true, resetNext = false, cannotEquals = true;
+    items.forEach(dispToInput);
+
+    function dispToInput(el) {
+        el.addEventListener('click', function() {
+            if (el.textContent === 'C' || input.value === "bruh") {
+                input.value = '';
+                updateDispValue = true;
+                resetNext = false;
+                cannotEquals = true;
+            } else if (el.classList.contains('operator')) {
+                if (el.textContent === '=') {
+                    if (!cannotEquals) {
+                        input.value = operate(operation, Number(dispValue), Number(input.value));
+                        dispValue = input.value;
+                        updateDispValue = true;
+                    }
+                }
+                else if (!updateDispValue) {
+                    dispValue = operate(operation, Number(dispValue), Number(input.value));
+                    input.value = dispValue;
+                    resetNext = true;
+                    operation = el.textContent;
+                } else {
+                    input.value = '';
+                    operation = el.textContent;
+                    updateDispValue = false;
+                }
+                cannotEquals = false;
+            } else {
+                if (resetNext) {
+                    input.value = '';
+                    resetNext = false;
+                }
+                input.value = input.value + this.textContent;
+                if(updateDispValue) dispValue = input.value;
+            }
+        });
+    }
+}
 
 function add(a, b) {
     return a + b;
@@ -17,6 +59,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) return "bruh";
     return a / b;
 }
 
@@ -27,57 +70,3 @@ function operate(operator, a, b) {
     if (operator === '/') return divide(a, b);
 }
 
-function dispToInput(el) {
-    el.addEventListener('click', function() {
-        if (el.classList.contains('operator')) {
-            if (el.innerHTML === '=') {
-                if (!cannotEquals) {
-                    input.value = operate(operation, Number(dispValue), Number(input.value));
-                    dispValue = input.value;
-                    updateDispValue = true;
-                }
-            }
-            else if (!updateDispValue) {
-                dispValue = operate(operation, Number(dispValue), Number(input.value));
-                input.value = dispValue;
-                resetNext = true;
-                operation = el.textContent;
-            } else {
-                input.value = '';
-                operation = el.textContent;
-                updateDispValue = false;
-            }
-            cannotEquals = false;
-        } else {
-            if (resetNext) {
-                input.value = '';
-                resetNext = false;
-            }
-            input.value = input.value + this.textContent;
-            if(updateDispValue) dispValue = input.value;
-        }
-    });
-}
-
-
-// function dispToInput(el) {
-//     el.addEventListener('click', function() {
-//         if (el.classList.contains('operator')) {
-//             if (el.innerHTML === '=' || operationIsEquals) {
-//                 input.value = operate(operation, Number(dispValue), Number(input.value));
-//                 dispValue = input.value;
-//             }
-//             else if (!updateDispValue) {
-//                 dispValue = operate(operation, Number(dispValue), Number(input.value));
-//                 operationIsEquals = true;
-//             } else {
-//                 input.value = '';
-//                 operation = el.textContent;
-//                 updateDispValue = false;
-//             }
-//         } else {
-//             input.value = input.value + this.textContent;
-//             if(updateDispValue) dispValue = input.value;
-//         }
-//     });
-// }
