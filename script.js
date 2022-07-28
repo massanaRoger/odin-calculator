cc55004924d2f302b4c5e153acda30d343b647ec
Roger Massana <rogermassana2002@gmail.com>
2022-07-28 22:02:38 +0200

    Add keyboard support

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 start();
 
-document.addEventListener('keydown', );
 
-function scanKey(event) {
-
-}
 
 function start() {
     const items = document.querySelectorAll('.item');
@@ -14,42 +10,53 @@ function start() {
     let updateDispValue = true, resetNext = false, cannotEquals = true;
     items.forEach(dispToInput);
 
-    function dispToInput(el) {
-        el.addEventListener('click', function() {
-            if (el.textContent === 'C' || input.value === "bruh") {
-                input.value = '';
-                updateDispValue = true;
-                resetNext = false;
-                cannotEquals = true;
-            } else if (el.textContent === '.' && input.value.indexOf('.') > -1) {}
-            else if (el.classList.contains('operator')) {
-                if (el.textContent === '=') {
-                    if (!cannotEquals) {
-                        input.value = operate(operation, Number(dispValue), Number(input.value));
-                        dispValue = input.value;
-                        updateDispValue = true;
-                    }
+    document.addEventListener('keydown', scanKey);
+
+    function scanKey(event) {
+        const keyCode = event.keyCode;
+        const keyToPress = document.querySelector(`[data-key="${keyCode}"]`);
+        keyToPress.dispKeyPress = dispKeyPress;
+        keyToPress.dispKeyPress();
+    }
+
+    function dispKeyPress() {
+        if (this.textContent === 'C' || input.value === "bruh") {
+            input.value = '';
+            updateDispValue = true;
+            resetNext = false;
+            cannotEquals = true;
+        } else if (this.textContent === '.' && input.value.indexOf('.') > -1) {}
+        else if (this.classList.contains('operator')) {
+            if (this.textContent === '=') {
+                if (!cannotEquals) {
+                    input.value = operate(operation, Number(dispValue), Number(input.value));
+                    dispValue = input.value;
+                    updateDispValue = true;
                 }
-                else if (!updateDispValue) {
-                    dispValue = operate(operation, Number(dispValue), Number(input.value));
-                    input.value = dispValue;
-                    resetNext = true;
-                    operation = el.textContent;
-                } else {
-                    input.value = '';
-                    operation = el.textContent;
-                    updateDispValue = false;
-                }
-                cannotEquals = false;
-            } else {
-                if (resetNext) {
-                    input.value = '';
-                    resetNext = false;
-                }
-                input.value = input.value + this.textContent;
-                if(updateDispValue) dispValue = input.value;
             }
-        });
+            else if (!updateDispValue) {
+                dispValue = operate(operation, Number(dispValue), Number(input.value));
+                input.value = dispValue;
+                resetNext = true;
+                operation = this.textContent;
+            } else {
+                input.value = '';
+                operation = this.textContent;
+                updateDispValue = false;
+            }
+            cannotEquals = false;
+        } else {
+            if (resetNext) {
+                input.value = '';
+                resetNext = false;
+            }
+            input.value = input.value + this.textContent;
+            if(updateDispValue) dispValue = input.value;
+        }
+    }
+
+    function dispToInput(el) {
+        el.addEventListener('click', dispKeyPress);
     }
 }
 
